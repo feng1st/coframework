6c929b4ea57f162b42fa1be3978aa4ec99e19629
feng1st <nf_xp+github@hotmail.com>
2023-06-09 19:15:18 +0800

    Spring generated project structure

---
+++ src/test/java/io/codeone/framework/CoframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codeone.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoframeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
