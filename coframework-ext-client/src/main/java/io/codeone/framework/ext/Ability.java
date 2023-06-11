fcf49c880731bec0c00649a0659d130d136dd7c3
feng1st <nf_xp+github@hotmail.com>
2023-06-11 11:14:02 +0800

    Add some comments.

--- coframework-ext-client/src/main/java/io/codeone/framework/ext/Ability.java
+++ coframework-ext-client/src/main/java/io/codeone/framework/ext/Ability.java
@@ -2,13 +2,30 @@ package io.codeone.framework.ext;
 
 import java.lang.annotation.*;
 
+/**
+ * Ability is an abstraction that indicates what tasks a system can complete. The system can be designed to provide
+ * Services with Abilities or Ability as a Service.
+ * <p>
+ * Ability annotation is used on interfaces, which represent the abilities themselves. The methods of the interface can
+ * represent sub-abilities, steps of the ability, extension points of the ability, etc.
+ * <p>
+ * Ability is Extensible, please refer to the Extensible annotation for more information.
+ *
+ * @see Extensible
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Extensible
 public @interface Ability {
 
+    /**
+     * A human-readable name of this Ability.
+     */
     String name() default "";
 
+    /**
+     * Description of this Ability.
+     */
     String description() default "";
 }
