b2596733e7b6eac9756f20aeec80987a345076a0
feng1st <nf_xp+github@hotmail.com>
2023-06-18 20:44:06 +0800

    Add comments.

--- coframework-ext-client/src/main/java/io/codeone/framework/ext/BizScenarioParam.java
+++ coframework-ext-client/src/main/java/io/codeone/framework/ext/BizScenarioParam.java
@@ -3,7 +3,8 @@ package io.codeone.framework.ext;
 /**
  * The interface of any parameter which has a BizScenario. The framework uses
  * this type to look up an Extension (one particular implementation) of an
- * Extensible (an interface which can be implemented differently).
+ * Extensible (an interface which can be implemented differently for different
+ * BizScenarios).
  */
 public interface BizScenarioParam {
 
