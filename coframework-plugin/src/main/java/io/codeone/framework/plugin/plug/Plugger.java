a552621a1e3164c5711fbcc8279ad4904d8594e5
feng1st <nf_xp+github@hotmail.com>
2023-07-02 00:14:39 +0800

    Minor change

--- coframework-plugin/src/main/java/io/codeone/framework/plugin/plug/Plugger.java
+++ coframework-plugin/src/main/java/io/codeone/framework/plugin/plug/Plugger.java
@@ -5,9 +5,19 @@ import io.codeone.framework.plugin.Plugin;
 import java.lang.reflect.Method;
 import java.util.List;
 
+/**
+ * A Plugger is the one who decides whether a method is plugged and what
+ * plugins are plugged in.
+ */
 public interface Plugger {
 
+    /**
+     * Returns whether the method is plugged.
+     */
     boolean isPlugged(Method method);
 
+    /**
+     * Returns what plugins are plugged into the method.
+     */
     List<Plugin<?>> getPlugins(Method method);
 }
