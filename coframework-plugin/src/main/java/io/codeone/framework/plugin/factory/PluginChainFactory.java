8af944d3f1f93d69037a56a2c4bcc53427ccc1c3
feng1st <nf_xp+github@hotmail.com>
2023-07-01 23:42:01 +0800

    Minor change

--- coframework-plugin/src/main/java/io/codeone/framework/plugin/factory/PluginChainFactory.java
+++ coframework-plugin/src/main/java/io/codeone/framework/plugin/factory/PluginChainFactory.java
@@ -1,13 +1,17 @@
 package io.codeone.framework.plugin.factory;
 
+import io.codeone.framework.plugin.Plugin;
 import io.codeone.framework.plugin.plug.Pluggers;
 import io.codeone.framework.plugin.util.PluginChain;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 @Component
 public class PluginChainFactory {
@@ -15,11 +19,21 @@ public class PluginChainFactory {
     @Resource
     private Pluggers pluggers;
 
-    private final Map<Method, PluginChain> methodChainMap
+    private final Map<Method, PluginChain> chainMap
+            = new ConcurrentHashMap<>();
+
+    private final Map<Set<Class<?>>, PluginChain> pluginTypeMap
             = new ConcurrentHashMap<>();
 
     public PluginChain getChain(Method method) {
-        return methodChainMap.computeIfAbsent(method,
-                k -> new PluginChain(pluggers.getPlugins(method)));
+        return chainMap.computeIfAbsent(method,
+                k1 -> {
+                    List<Plugin<?>> plugins = pluggers.getPlugins(method);
+                    Set<Class<?>> pluginTypes = plugins.stream()
+                            .map(Object::getClass)
+                            .collect(Collectors.toSet());
+                    return pluginTypeMap.computeIfAbsent(pluginTypes,
+                            k2 -> new PluginChain(plugins));
+                });
     }
 }
