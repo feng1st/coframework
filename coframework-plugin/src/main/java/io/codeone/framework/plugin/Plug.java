f24ace6b1620a8c277febd1d69d2dcf1d2758ff9
feng1st <nf_xp+github@hotmail.com>
2023-07-05 00:27:23 +0800

    Add comments

--- coframework-plugin/src/main/java/io/codeone/framework/plugin/Plug.java
+++ coframework-plugin/src/main/java/io/codeone/framework/plugin/Plug.java
@@ -5,9 +5,7 @@ import org.springframework.stereotype.Component;
 import java.lang.annotation.*;
 
 /**
- * Applied on 'Plugin', to specify an order.
- * <p>
- * Plugins will be executed first by stage then by order.
+ * Applied on 'Plugin', to specify an order and the group it belongs.
  *
  * @see Plugin
  */
@@ -19,11 +17,22 @@ import java.lang.annotation.*;
 public @interface Plug {
 
     /**
-     * The stage the plugin belongs to.
+     * The stage the plugin belongs to. Plugins will be sorted by stage in a
+     * chain.
+     * <p>
+     * You can use the '@Order' annotation to further specify the order of
+     * plugins that belong to the same stage, if that matter.
      *
      * @see Stages
      */
     Stages value() default Stages.BEFORE_TARGET;
 
+    /**
+     * The group the plugin belongs to. Use group to select multiple plugins at
+     * once.
+     * <p>
+     * Empty value means the plugin does not belong to any group. In that case
+     * you can select them only by specifying the individual classes of them.
+     */
     String group() default "";
 }
