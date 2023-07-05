d6a175634ca28bece48d18c83c5a1316ce17b20a
feng1st <nf_xp+github@hotmail.com>
2023-07-05 21:30:08 +0800

    Add auto configuration

---
+++ coframework-plugin/src/main/java/io/codeone/framework/plugin/config/PluginAutoConfiguration.java
@@ -0,0 +1,9 @@
+package io.codeone.framework.plugin.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("io.codeone.framework")
+public class PluginAutoConfiguration {
+}
