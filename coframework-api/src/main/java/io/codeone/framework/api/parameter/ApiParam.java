caf2121bf73cc31e6146b30a8e8f10036e78c31d
feng1st <nf_xp+github@hotmail.com>
2024-11-26 20:28:48 +0800

    Refactoring

--- coframework-api/src/main/java/io/codeone/framework/api/parameter/ApiParam.java
+++ coframework-api/src/main/java/io/codeone/framework/api/parameter/ApiParam.java
@@ -1,6 +1,21 @@
 package io.codeone.framework.api.parameter;
 
+/**
+ * Represents a parameter that requires validation.
+ *
+ * <p>Parameters are automatically validated by the framework if the associated
+ * services or methods are annotated with {@code API}.
+ */
 public interface ApiParam {
 
+    /**
+     * Validates the parameter, throwing an {@code IllegalArgumentException} if
+     * invalid.
+     *
+     * <p>Use this method to ensure parameters meet all necessary conditions before
+     * processing.
+     *
+     * @see io.codeone.framework.api.util.Validator
+     */
     void validate();
 }
