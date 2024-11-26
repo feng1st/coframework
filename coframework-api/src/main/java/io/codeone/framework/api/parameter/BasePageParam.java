521c1093dd3169d23d0bcfe009c82bfa1013728b
feng1st <nf_xp+github@hotmail.com>
2024-11-26 20:51:53 +0800

    Refactoring

--- coframework-api/src/main/java/io/codeone/framework/api/parameter/BasePageParam.java
+++ coframework-api/src/main/java/io/codeone/framework/api/parameter/BasePageParam.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
+/**
+ * Base class for pagination parameters.
+ */
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
@@ -14,14 +17,29 @@ public abstract class BasePageParam extends BaseParam {
 
     private int pageSize = PageConstants.DEFAULT_PAGE_SIZE;
 
+    /**
+     * Sets the page index, ensuring it is not below the starting page index.
+     *
+     * @param pageIndex the page index to set
+     */
     public void setPageIndex(int pageIndex) {
         this.pageIndex = Math.max(pageIndex, PageConstants.START_PAGE_INDEX);
     }
 
+    /**
+     * Sets the page size, ensuring it is not below the minimum page size.
+     *
+     * @param pageSize the page size to set
+     */
     public void setPageSize(int pageSize) {
         this.pageSize = Math.max(pageSize, PageConstants.MIN_PAGE_SIZE);
     }
 
+    /**
+     * Calculates the offset based on the current page index and page size.
+     *
+     * @return the offset value
+     */
     public long getOffset() {
         return ((long) this.pageIndex - PageConstants.START_PAGE_INDEX) * this.pageSize;
     }
