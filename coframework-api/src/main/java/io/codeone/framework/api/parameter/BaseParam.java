f464889c25c9ca691e048f3fab1a51a12569332a
feng1st <nf_xp+github@hotmail.com>
2024-11-26 20:47:23 +0800

    Refactoring

--- coframework-api/src/main/java/io/codeone/framework/api/parameter/BaseParam.java
+++ coframework-api/src/main/java/io/codeone/framework/api/parameter/BaseParam.java
@@ -4,15 +4,32 @@ import io.codeone.framework.ext.BizScenario;
 import io.codeone.framework.ext.BizScenarioParam;
 import lombok.Data;
 
+/**
+ * Base class for parameters that:
+ * <ul>
+ *   <li>Implements {@link ApiParam}, allowing automatic validation by the framework.</li>
+ *   <li>Implements {@link BizScenarioParam}, which can be used to route to a specific
+ *   implementation of an {@code Extensible} interface when the {@code bizScenario}
+ *   is present.</li>
+ * </ul>
+ */
 @Data
 public abstract class BaseParam implements ApiParam, BizScenarioParam {
 
     private BizScenario bizScenario;
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void validate() {
     }
 
+    /**
+     * Retrieves the business ID.
+     *
+     * @return the business ID
+     */
     public String getBizId() {
         if (bizScenario == null) {
             return null;
@@ -20,6 +37,11 @@ public abstract class BaseParam implements ApiParam, BizScenarioParam {
         return bizScenario.getBizId();
     }
 
+    /**
+     * Sets the business ID.
+     *
+     * @param bizId the business ID to set
+     */
     public void setBizId(String bizId) {
         if (bizScenario == null) {
             bizScenario = BizScenario.ofBizId(bizId);
@@ -28,6 +50,11 @@ public abstract class BaseParam implements ApiParam, BizScenarioParam {
         }
     }
 
+    /**
+     * Retrieves the scenario.
+     *
+     * @return the scenario
+     */
     public String getScenario() {
         if (bizScenario == null) {
             return null;
@@ -35,6 +62,11 @@ public abstract class BaseParam implements ApiParam, BizScenarioParam {
         return bizScenario.getScenario();
     }
 
+    /**
+     * Sets the scenario.
+     *
+     * @param scenario the scenario to set
+     */
     public void setScenario(String scenario) {
         if (bizScenario == null) {
             bizScenario = BizScenario.ofScenario(scenario);
