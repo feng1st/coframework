5e3e07b9bfa38da062f1330f3f2230b8abdbb19c
feng1st <nf_xp+github@hotmail.com>
2024-11-26 22:56:16 +0800

    Refactoring

--- coframework-api/src/main/java/io/codeone/framework/api/exception/ApiException.java
+++ coframework-api/src/main/java/io/codeone/framework/api/exception/ApiException.java
@@ -3,9 +3,12 @@ package io.codeone.framework.api.exception;
 /**
  * Represents an exception with an error code for service operations.
  *
- * <p>Service operation exceptions will be recognized and logged by the framework
- * if the type is {@code ApiException} or can be converted to {@code ApiException}
- * using {@code ApiExceptionConverter}.
+ * <p>If a service or method is annotated with {@code API}, any exception that is
+ * {@code ApiException} or can be converted into {@code ApiException} using {@code ApiExceptionConverter}
+ * will be transformed by the framework into a failed result with a specified code.
+ *
+ * <p>Additionally, if the service or method is annotated with {@code API} or {@code Logging},
+ * any such exception's code will be recognized and logged by the framework.
  */
 public interface ApiException {
 
--- coframework-api/src/main/java/io/codeone/framework/api/response/ApiResult.java
+++ coframework-api/src/main/java/io/codeone/framework/api/response/ApiResult.java
@@ -3,9 +3,10 @@ package io.codeone.framework.api.response;
 /**
  * Represents the result of a service operation.
  *
- * <p>Service operation results, including success status, data, error code, and
- * error message, will be recognized and logged by the framework if the type is
- * {@code ApiResult} or can be converted to {@code ApiResult} using {@code ApiResultConverter}.
+ * <p>If a service or method is annotated with {@code API} or {@code Logging}, and
+ * the return type is {@code ApiResult} or can be converted to {@code ApiResult}
+ * using {@code ApiResultConverter}, the result's success status, data, error code,
+ * and error message will be automatically recognized and logged by the framework.
  *
  * @param <T> the type of data returned by the operation
  */
