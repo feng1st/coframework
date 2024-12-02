8182383306b01e751709ded38f11601c4bca99ee
feng1st <nf_xp+github@hotmail.com>
2024-12-02 23:00:13 +0800

    Refactoring

--- coframework-ext-client/src/main/java/io/codeone/framework/ext/BizScenario.java
+++ coframework-ext-client/src/main/java/io/codeone/framework/ext/BizScenario.java
@@ -8,6 +8,49 @@ import lombok.Getter;
 import java.beans.Transient;
 import java.util.Iterator;
 
+/**
+ * Represents an identifier for routing to a specific {@code Extension} implementation
+ * of an {@code Extensible} interface.
+ *
+ * <p>An {@code Extensible} interface is an interface annotated with {@code Ability}
+ * or {@code ExtensionPoint}, which defines a contract for extensible behavior.
+ * An {@code Extension} implementation is a concrete implementation of an {@code
+ * Extensible} interface, annotated with {@code Extension}, that specifies its identifier
+ * as a coordinate using business identity ({@code bizId}) and scenario.
+ *
+ * <p>A call to an {@code Extensible} interface can automatically locate and execute
+ * a specific {@code Extension} implementation based on the provided or contextual
+ * {@code BizScenario}.
+ *
+ * <p>Examples:
+ * <ul>
+ *   <li>{@code @Ability
+ *   interface Work { void execute(BizScenario bizScenario); }}
+ *   <li>{@code @Extension(bizId = "Sales", scenario = "weekday.morning")
+ *   class WorkInSalesMorning implements Work {}}
+ *   <li>{@code @Extension(bizId = "Sales", scenario = "weekday")
+ *   class WorkInSalesWeekday implements Work {}}
+ *   <li>{@code @Extension(bizId = "HR", scenario = "weekday.morning")
+ *   class WorkInHRMorning implements Work {}}
+ * </ul>
+ *
+ * <p>Routing examples:
+ * <ul>
+ *   <li>{@code work.execute(BizScenario.of("Sales", "weekday.morning"))} will route
+ *   to {@code WorkInSalesMorning}.
+ *   <li>{@code work.execute(BizScenario.of("Sales", "weekday.evening"))} will route
+ *   to {@code WorkInSalesWeekday}, as the exact scenario match is not found, but
+ *   its hierarchical traversal identifies {@code BizScenario.of("Sales", "weekday")}.
+ *   <li>{@code work.execute(BizScenario.of("HR", "weekday.morning"))} will route
+ *   to {@code WorkInHRMorning}.
+ * </ul>
+ *
+ * <p>This class provides utilities for defining and manipulating a combination
+ * of business identity and scenario. It supports hierarchical traversal of both
+ * {@code bizId} and {@code scenario}, such as moving from specific levels (e.g.,
+ * {@code "region.branch"}) to broader levels (e.g., {@code "region"} or the wildcard
+ * {@code "*"}).
+ */
 @Getter
 @EqualsAndHashCode
 public class BizScenario implements BizScenarioParam, Iterable<BizScenario> {
@@ -16,26 +59,72 @@ public class BizScenario implements BizScenarioParam, Iterable<BizScenario> {
 
     private String scenario = BizScenarioUtils.ANY;
 
+    /**
+     * Creates a new {@code BizScenario} with the specified business identity.
+     *
+     * @param bizId the business identity
+     * @return a new {@code BizScenario} representing the business identity and
+     * scenario
+     */
     public static BizScenario ofBizId(String... bizId) {
         return of(BizScenarioUtils.join(bizId), BizScenarioUtils.ANY);
     }
 
+    /**
+     * Creates a new {@code BizScenario} with the specified scenario.
+     *
+     * @param scenario the scenario
+     * @return a new {@code BizScenario} representing the business identity and
+     * scenario
+     */
     public static BizScenario ofScenario(String... scenario) {
         return of(BizScenarioUtils.ANY, BizScenarioUtils.join(scenario));
     }
 
+    /**
+     * FIXME
+     * Creates a new {@code BizScenario} with multiple business identities and scenarios.
+     *
+     * @param bizId    an array of business identities
+     * @param scenario an array of scenarios
+     * @return a new {@code BizScenario} representing the business identity and scenario
+     */
     public static BizScenario of(String[] bizId, String[] scenario) {
         return of(BizScenarioUtils.join(bizId), BizScenarioUtils.join(scenario));
     }
 
+    /**
+     * FIXME
+     * Creates a new {@code BizScenario} with multiple business identities and a single scenario.
+     *
+     * @param bizId    an array of business identities
+     * @param scenario a single scenario
+     * @return a new {@code BizScenario} representing the business identity and scenario
+     */
     public static BizScenario of(String[] bizId, String scenario) {
         return of(BizScenarioUtils.join(bizId), scenario);
     }
 
+    /**
+     * FIXME
+     * Creates a new {@code BizScenario} with a single business identity and multiple scenarios.
+     *
+     * @param bizId    a single business identity
+     * @param scenario an array of scenarios
+     * @return a new {@code BizScenario} representing the business identity and scenario
+     */
     public static BizScenario of(String bizId, String[] scenario) {
         return of(bizId, BizScenarioUtils.join(scenario));
     }
 
+    /**
+     * FIXME
+     * Creates a new {@code BizScenario} with a single business identity and scenario.
+     *
+     * @param bizId    a single business identity
+     * @param scenario a single scenario
+     * @return a new {@code BizScenario} representing the business identity and scenario
+     */
     public static BizScenario of(String bizId, String scenario) {
         BizScenario bizScenario = new BizScenario();
         bizScenario.setBizId(bizId);
@@ -43,43 +132,85 @@ public class BizScenario implements BizScenarioParam, Iterable<BizScenario> {
         return bizScenario;
     }
 
+    // FIXME
     public BizScenario withBizId(String... bizId) {
         return of(BizScenarioUtils.join(bizId), this.scenario);
     }
 
+    // FIXME
     public BizScenario withScenario(String... scenario) {
         return of(this.bizId, BizScenarioUtils.join(scenario));
     }
 
+    // FIXME
     public BizScenario prependScenario(String scenario) {
         return withScenario(scenario, this.scenario);
     }
 
+    // FIXME
     public BizScenario appendScenario(String scenario) {
         return of(this.scenario, scenario);
     }
 
+    /**
+     * Sets the business identity for this {@code BizScenario}.
+     *
+     * <p>The provided business identity is validated and normalized. If it is empty
+     * or null, it is replaced with the wildcard {@code "*"}.
+     *
+     * @param bizId the business identity
+     */
     public void setBizId(String bizId) {
         BizScenarioUtils.validate(bizId);
         this.bizId = BizScenarioUtils.emptyToAny(bizId);
     }
 
+    /**
+     * Sets the scenario for this {@code BizScenario}.
+     *
+     * <p>The provided scenario is validated and normalized. If it is empty or null,
+     * it is replaced with the wildcard {@code "*"}.
+     *
+     * @param scenario the scenario
+     */
     public void setScenario(String scenario) {
         BizScenarioUtils.validate(scenario);
         this.scenario = BizScenarioUtils.emptyToAny(scenario);
     }
 
+    /**
+     * Returns a string representation of the business identity and scenario.
+     *
+     * <p>The format of the string is {@code "bizId|scenario"}.
+     *
+     * @return a string representation of this {@code BizScenario}
+     */
     @Override
     public String toString() {
         return bizId + "|" + scenario;
     }
 
+    /**
+     * Retrieves the current {@code BizScenario} instance.
+     *
+     * @return the current {@code BizScenario} instance
+     */
     @Override
     @Transient
     public BizScenario getBizScenario() {
         return this;
     }
 
+    /**
+     * Returns an iterator to traverse hierarchical levels of both {@code bizId}
+     * and {@code scenario} in this {@code BizScenario}.
+     *
+     * <p>The iteration starts from the most specific levels (e.g., {@code "region.branch"
+     * or "weekday.morning"}) and moves to broader levels (e.g., {@code "region"
+     * or "weekday"}), eventually reaching the wildcard {@code "*"}.
+     *
+     * @return an iterator for hierarchical traversal of this {@code BizScenario}
+     */
     @Override
     public Iterator<BizScenario> iterator() {
         return new BizScenarioIterator(this);
