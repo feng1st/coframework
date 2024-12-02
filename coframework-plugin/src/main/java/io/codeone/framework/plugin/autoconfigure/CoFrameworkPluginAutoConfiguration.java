4e210b38e520c8e61c0f34de0831ed995883bc96
feng1st <nf_xp+github@hotmail.com>
2024-12-02 13:07:02 +0800

    Refactoring

--- coframework-plugin/src/main/java/io/codeone/framework/plugin/autoconfigure/CoFrameworkPluginAutoConfiguration.java
+++ coframework-plugin/src/main/java/io/codeone/framework/plugin/autoconfigure/CoFrameworkPluginAutoConfiguration.java
@@ -8,12 +8,20 @@ import org.springframework.core.io.support.SpringFactoriesLoader;
 
 import java.util.List;
 
+/**
+ * Autoconfiguration for Co-Framework plugins.
+ *
+ * <p>This configuration class scans and registers plugin-related components. It
+ * also loads all {@link AnnoPluginBindingFactory} implementations using {@code
+ * SpringFactoriesLoader} and populates the {@link AnnoPluginBindingRepo} with their
+ * provided bindings.
+ */
 @Configuration
 @ComponentScan("io.codeone.framework.plugin")
 public class CoFrameworkPluginAutoConfiguration {
 
     static {
-
+        // Load all AnnoPluginBindingFactory implementations and populate the repository
         List<AnnoPluginBindingFactory> factories = SpringFactoriesLoader.loadFactories(
                 AnnoPluginBindingFactory.class, null);
 
--- coframework-plugin/src/main/java/io/codeone/framework/plugin/binding/AnnoPluginBindingFactory.java
+++ coframework-plugin/src/main/java/io/codeone/framework/plugin/binding/AnnoPluginBindingFactory.java
@@ -2,7 +2,19 @@ package io.codeone.framework.plugin.binding;
 
 import java.util.List;
 
+/**
+ * A factory interface for creating annotation-to-plugin bindings.
+ *
+ * <p>Implementations of this interface provide a list of {@link AnnoPluginBinding}
+ * instances that define how specific annotations are associated with plugins. These
+ * factories are discovered and invoked by {@code CoFrameworkPluginAutoConfiguration}.
+ */
 public interface AnnoPluginBindingFactory {
 
+    /**
+     * Retrieves the list of annotation-to-plugin bindings.
+     *
+     * @return a list of {@link AnnoPluginBinding} instances
+     */
     List<AnnoPluginBinding> getBindings();
 }
