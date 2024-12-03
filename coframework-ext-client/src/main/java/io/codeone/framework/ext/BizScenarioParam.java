ec4ff2aad21f4dab6362b1ab193ec28b0d61f28d
feng1st <nf_xp+github@hotmail.com>
2024-12-03 19:18:29 +0800

    Refactoring

--- coframework-ext-client/src/main/java/io/codeone/framework/ext/BizScenarioParam.java
+++ coframework-ext-client/src/main/java/io/codeone/framework/ext/BizScenarioParam.java
@@ -1,6 +1,18 @@
 package io.codeone.framework.ext;
 
+/**
+ * Defines a contract for providing a {@code BizScenario} instance.
+ *
+ * <p>Classes implementing this interface allow the framework to extract a {@code
+ * BizScenario} that can be used to locate a specific {@code Extension} implementation
+ * of an {@code Extensible} interface.
+ */
 public interface BizScenarioParam {
 
+    /**
+     * Retrieves the {@code BizScenario} associated with this instance.
+     *
+     * @return the associated {@code BizScenario}
+     */
     BizScenario getBizScenario();
 }
