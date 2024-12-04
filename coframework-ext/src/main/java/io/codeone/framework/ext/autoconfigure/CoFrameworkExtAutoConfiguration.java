ebbe69b510ed5f99f1bc68a3f4d4c0c2f87c8773
feng1st <nf_xp+github@hotmail.com>
2024-12-04 23:10:01 +0800

    Refactoring

--- coframework-ext/src/main/java/io/codeone/framework/ext/autoconfigure/CoFrameworkExtAutoConfiguration.java
+++ coframework-ext/src/main/java/io/codeone/framework/ext/autoconfigure/CoFrameworkExtAutoConfiguration.java
@@ -3,6 +3,9 @@ package io.codeone.framework.ext.autoconfigure;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Autoconfiguration for the Co-Framework extension components.
+ */
 @Configuration
 @ComponentScan("io.codeone.framework.ext")
 public class CoFrameworkExtAutoConfiguration {
