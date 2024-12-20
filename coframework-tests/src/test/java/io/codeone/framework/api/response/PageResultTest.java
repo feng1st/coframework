5ab94eafa4c745e8482bf9194a2967b17eac5670
feng1st <nf_xp+github@hotmail.com>
2024-12-20 21:40:54 +0800

    Refactoring

---
+++ coframework-tests/src/test/java/io/codeone/framework/api/response/PageResultTest.java
@@ -0,0 +1,5 @@
+package io.codeone.framework.api.response;
+
+class PageResultTest {
+
+}
---
+++ coframework-tests/src/test/java/io/codeone/framework/api/response/PageTest.java
@@ -0,0 +1,87 @@
+package io.codeone.framework.api.response;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class PageTest {
+
+    @Test
+    public void invalidPageIndexSize() {
+        Page<Object> page = Page.of(0, 0);
+        Assertions.assertNull(page.getData());
+        Assertions.assertEquals(1, page.getPageIndex());
+        Assertions.assertEquals(1, page.getPageSize());
+        Assertions.assertNull(page.getTotalCount());
+        Assertions.assertTrue(page.isEmpty());
+        Assertions.assertFalse(page.isHasMore());
+    }
+
+    @Test
+    public void ofPageData() {
+        PageData<Object> pageData = Page.of(Arrays.asList(1, 2, 3), 5, 3, 100L);
+        Page<Object> page = Page.of(pageData);
+        Assertions.assertEquals(Arrays.asList(1, 2, 3), page.getData());
+        Assertions.assertEquals(5, page.getPageIndex());
+        Assertions.assertEquals(3, page.getPageSize());
+        Assertions.assertEquals(100L, page.getTotalCount());
+        Assertions.assertFalse(page.isEmpty());
+        Assertions.assertTrue(page.isHasMore());
+    }
+
+    @Test
+    public void ofPageIndexSize() {
+        Page<Object> page = Page.of(5, 10);
+        Assertions.assertNull(page.getData());
+        Assertions.assertEquals(5, page.getPageIndex());
+        Assertions.assertEquals(10, page.getPageSize());
+        Assertions.assertNull(page.getTotalCount());
+        Assertions.assertTrue(page.isEmpty());
+        Assertions.assertFalse(page.isHasMore());
+    }
+
+    @Test
+    public void ofDataPageIndexSize() {
+        Page<Object> page = Page.of(Arrays.asList(1, 2, 3), 5, 10);
+        Assertions.assertEquals(Arrays.asList(1, 2, 3), page.getData());
+        Assertions.assertEquals(5, page.getPageIndex());
+        Assertions.assertEquals(10, page.getPageSize());
+        Assertions.assertNull(page.getTotalCount());
+        Assertions.assertFalse(page.isEmpty());
+        Assertions.assertFalse(page.isHasMore());
+    }
+
+    @Test
+    public void ofDataPageIndexSizeTotalCount() {
+        Page<Object> page = Page.of(Arrays.asList(1, 2, 3), 5, 3, 100L);
+        Assertions.assertEquals(Arrays.asList(1, 2, 3), page.getData());
+        Assertions.assertEquals(5, page.getPageIndex());
+        Assertions.assertEquals(3, page.getPageSize());
+        Assertions.assertEquals(100L, page.getTotalCount());
+        Assertions.assertFalse(page.isEmpty());
+        Assertions.assertTrue(page.isHasMore());
+    }
+
+    @Test
+    public void hasMore() {
+        Page<Object> page = Page.of(Arrays.asList(1, 2, 3), 5, 3, 16L);
+        Assertions.assertEquals(Arrays.asList(1, 2, 3), page.getData());
+        Assertions.assertEquals(5, page.getPageIndex());
+        Assertions.assertEquals(3, page.getPageSize());
+        Assertions.assertEquals(16L, page.getTotalCount());
+        Assertions.assertFalse(page.isEmpty());
+        Assertions.assertTrue(page.isHasMore());
+    }
+
+    @Test
+    public void hasNoMore() {
+        Page<Object> page = Page.of(Arrays.asList(1, 2, 3), 5, 3, 15L);
+        Assertions.assertEquals(Arrays.asList(1, 2, 3), page.getData());
+        Assertions.assertEquals(5, page.getPageIndex());
+        Assertions.assertEquals(3, page.getPageSize());
+        Assertions.assertEquals(15L, page.getTotalCount());
+        Assertions.assertFalse(page.isEmpty());
+        Assertions.assertFalse(page.isHasMore());
+    }
+}
