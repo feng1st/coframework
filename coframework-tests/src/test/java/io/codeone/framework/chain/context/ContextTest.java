7fb5543d0361d46f5c33d1ff81c5a66129cf8189
feng1st <nf_xp+github@hotmail.com>
2024-12-22 19:34:17 +0800

    Refactoring

--- coframework-tests/src/test/java/io/codeone/framework/chain/context/ContextTest.java
+++ coframework-tests/src/test/java/io/codeone/framework/chain/context/ContextTest.java
@@ -85,22 +85,124 @@ class ContextTest {
     @Test
     public void putAndGetTypedInvalid() {
         Context context = Context.of();
-        context.put(ContextTestTyped.INT, 1);
-        Assertions.assertThrows(ClassCastException.class, () -> context.<String>ifPresent(ContextTestTyped.INT, o -> {
+        Typed key = ContextTestTyped.INT;
+        context.put(key, 1);
+        Assertions.assertThrows(ClassCastException.class, () -> context.<String>ifPresent(key, o -> {
         }));
-        Assertions.assertThrows(ClassCastException.class, () -> context.getOrDefault(ContextTestTyped.INT, "value"));
-        Assertions.assertThrows(ClassCastException.class, () -> context.put(ContextTestTyped.INT, "value"));
-        Assertions.assertThrows(ClassCastException.class, () -> context.putIfAbsent(ContextTestTyped.INT, "value"));
+        Assertions.assertThrows(ClassCastException.class, () -> context.getOrDefault(key, "value"));
+        Assertions.assertThrows(ClassCastException.class, () -> context.put(key, "value"));
+        Assertions.assertThrows(ClassCastException.class, () -> context.putIfAbsent(key, "value"));
     }
 
     @Test
     public void putAndGetClassInvalid() {
         Context context = Context.of();
-        context.put(Integer.class, 1);
-        Assertions.assertThrows(ClassCastException.class, () -> context.<String>ifPresent(Integer.class, o -> {
+        Class<Integer> key = Integer.class;
+        context.put(key, 1);
+        Assertions.assertThrows(ClassCastException.class, () -> context.<String>ifPresent(key, o -> {
         }));
-        Assertions.assertThrows(ClassCastException.class, () -> context.getOrDefault(Integer.class, "value"));
-        Assertions.assertThrows(ClassCastException.class, () -> context.put(Integer.class, "value"));
-        Assertions.assertThrows(ClassCastException.class, () -> context.putIfAbsent(Integer.class, "value"));
+        Assertions.assertThrows(ClassCastException.class, () -> context.getOrDefault(key, "value"));
+        Assertions.assertThrows(ClassCastException.class, () -> context.put(key, "value"));
+        Assertions.assertThrows(ClassCastException.class, () -> context.putIfAbsent(key, "value"));
+    }
+
+    @Test
+    public void compute() {
+        Context context = Context.of();
+        Object key = new Object();
+        Assertions.assertNull(context.computeIfPresent(key, (k, v) -> {
+            Assertions.assertNull(v);
+            return "value";
+        }));
+        Assertions.assertNull(context.get(key));
+        Assertions.assertEquals("value", context.computeIfAbsent(key, k -> "value"));
+        Assertions.assertEquals("value", context.get(key));
+        Assertions.assertEquals("new value", context.computeIfPresent(key, (k, v) -> {
+            Assertions.assertEquals("value", v);
+            return "new value";
+        }));
+        Assertions.assertEquals("new value", context.get(key));
+        Assertions.assertNull(context.compute(key, (k, v) -> {
+            Assertions.assertEquals("new value", v);
+            return null;
+        }));
+        Assertions.assertEquals("value", context.compute(key, (k, v) -> {
+            Assertions.assertNull(v);
+            return "value";
+        }));
+        Assertions.assertEquals("new value", context.compute(key, (k, v) -> {
+            Assertions.assertEquals("value", v);
+            return "new value";
+        }));
+    }
+
+    @Test
+    public void computeTyped() {
+        Context context = Context.of();
+        Typed key = ContextTestTyped.STR;
+        Assertions.assertNull(context.computeIfPresent(key, (k, v) -> {
+            Assertions.assertNull(v);
+            return "value";
+        }));
+        Assertions.assertNull(context.get(key));
+        Assertions.assertEquals("value", context.computeIfAbsent(key, k -> "value"));
+        Assertions.assertEquals("value", context.get(key));
+        Assertions.assertEquals("new value", context.computeIfPresent(key, (k, v) -> {
+            Assertions.assertEquals("value", v);
+            return "new value";
+        }));
+        Assertions.assertEquals("new value", context.get(key));
+        Assertions.assertNull(context.compute(key, (k, v) -> {
+            Assertions.assertEquals("new value", v);
+            return null;
+        }));
+        Assertions.assertEquals("value", context.compute(key, (k, v) -> {
+            Assertions.assertNull(v);
+            return "value";
+        }));
+        Assertions.assertEquals("new value", context.compute(key, (k, v) -> {
+            Assertions.assertEquals("value", v);
+            return "new value";
+        }));
+    }
+
+    @Test
+    public void computeClass() {
+        Context context = Context.of();
+        Class<String> key = String.class;
+        Assertions.assertNull(context.computeIfPresent(key, (k, v) -> {
+            Assertions.assertNull(v);
+            return "value";
+        }));
+        Assertions.assertNull(context.get(key));
+        Assertions.assertEquals("value", context.computeIfAbsent(key, k -> "value"));
+        Assertions.assertEquals("value", context.get(key));
+        Assertions.assertEquals("new value", context.computeIfPresent(key, (k, v) -> {
+            Assertions.assertEquals("value", v);
+            return "new value";
+        }));
+        Assertions.assertEquals("new value", context.get(key));
+        Assertions.assertNull(context.compute(key, (k, v) -> {
+            Assertions.assertEquals("new value", v);
+            return null;
+        }));
+        Assertions.assertEquals("value", context.compute(key, (k, v) -> {
+            Assertions.assertNull(v);
+            return "value";
+        }));
+        Assertions.assertEquals("new value", context.compute(key, (k, v) -> {
+            Assertions.assertEquals("value", v);
+            return "new value";
+        }));
+    }
+
+    @Test
+    public void computeTypedInvalid() {
+        Context context = Context.of();
+        Typed key = ContextTestTyped.INT;
+        Assertions.assertThrows(ClassCastException.class, () -> context.computeIfAbsent(key, k -> "value"));
+        context.put(key, 1);
+        Assertions.assertThrows(ClassCastException.class, () -> context.computeIfPresent(key, (k, v) -> "value"));
+        Assertions.assertThrows(ClassCastException.class, () -> context.compute(key, (k, v) -> "value"));
     }
 }
