4d1d13da4e2a05fc838b8c68f100957a813499d4
feng1st <nf_xp+github@hotmail.com>
2025-01-15 21:05:24 +0800

    Refactoring

--- coframework-tests/src/test/java/io/codeone/framework/ext/session/ExtensionSessionTestAnnoPluginBindingFactory.java
+++ coframework-tests/src/test/java/io/codeone/framework/ext/session/ExtensionSessionTestAnnoPluginBindingFactory.java
@@ -2,12 +2,10 @@ package io.codeone.framework.ext.session;
 
 import io.codeone.framework.plugin.binding.AnnoPluginBinding;
 import io.codeone.framework.plugin.binding.AnnoPluginBindingFactory;
-import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
 
-@Component
 public class ExtensionSessionTestAnnoPluginBindingFactory implements AnnoPluginBindingFactory {
 
     @Override
